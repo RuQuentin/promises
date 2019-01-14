cf93fada50e634075a34946f1db4860c20d36af5
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-01-15 00:08:28 +0300

    feature/then, improve THEN method, adding possibility to work with situation, when THEN returns another promise

--- src/promise.js
+++ src/promise.js
@@ -1,6 +1,7 @@
-const PENDING = 'pending';
-const FULFILLED = 'fulfilled';
-const REJECTED = 'rejected';
+/*eslint-disable*/
+const RESOLVED = 'RESOLVED';
+const PENDING = 'PENDING';
+const REJECTED = 'REJECTED';
 
 class OwnPromise {
   constructor(executor) {
@@ -9,61 +10,163 @@ class OwnPromise {
     }
 
     this.state = PENDING;
-    this.value = undefined;
-    this.chained = [];
+    this.cbArray = [];
 
-    const resolve = value => {
+    // === helping function ===
+    const putProcessInLine = () => {
+      setTimeout(() => {
+        try {
+          executor(resolve, reject)
+        } catch (err) {
+          reject(err)
+        };
+      }, 0);
+    }
+    // ========================
+
+    const resolve = data => {
       if (this.state !== PENDING) {
-        return;
+        return
       }
-      this.state = FULFILLED;
-      this.value = value;
 
-      for (const { onFulfilled } of this.chained) {
-        onFulfilled(value);
+      if (!(data instanceof OwnPromise)) {
+        // console.log('step2 - 1st promise resolved');
+        this.state = RESOLVED;
+        this.value = data;
+      } else {
+        if (data.state === PENDING) {
+          putProcessInLine();
+        }
+
+        if (data.state === RESOLVED) {
+          if (!(data.value instanceof OwnPromise)) {
+            setTimeout(() => {
+              // console.log('step4 - then resolved')
+              this.state = RESOLVED;
+              this.value = data.cbArray.shift()['onResolve'](data.value);
+            }, 0);
+          } else {
+            if (data.value.state === PENDING) {
+              putProcessInLine();
+            } else {
+              this.state = RESOLVED;
+              this.value = data.cbArray.shift()['onResolve'](data.value.value);
+            }
+          }
+        }
       }
-    };
+    }
 
     const reject = error => {
       if (this.state !== PENDING) {
-        return;
+        return
+        // return res(this.value);
       }
+
       this.state = REJECTED;
       this.value = error;
-
-      for (const { onRejected } of this.chained) {
-        onRejected(error);
-      }
-    };
+    }
 
     try {
-      executor(resolve, reject);
+      executor(resolve, reject)
     } catch (err) {
-      reject(err);
+      reject(err)
+    };
+  }
+
+
+
+  // вспомогательная функция для проверки состояния, чтобы ее вызывать
+
+  then(onResolve, onReject) {
+    if (typeof onResolve !== 'function') {
+      throw new TypeError('onResolve must be a function');
+    }
+
+    this.cbArray.push({ onResolve, onReject });
+
+    if (this.state === RESOLVED || PENDING) {
+      // console.log('step3 - then creates new Promise');
+      return new OwnPromise(resolve => {
+        resolve(this);
+      });
     }
   }
 
-  static resolve(arg) {
-  }
+  catch(rej) {
 
-  static reject(arg) {
-  }
-
-  static race(arrOfPromises) {
-  }
-
-  static all(arrOfPromises) {
-  }
-
-  then(onFulfilled, onRejected) {
-    if (this.state === FULFILLED) {
-      onFulfilled(this.value);
-    } else if (this.state === REJECTED) {
-      onRejected(this.error);
-    } else {
-      this.chained.push({ onFulfilled, onRejected });
-    }
   }
 }
 
-module.exports = OwnPromise;
+// module.exports = OwnPromise;
+
+
+const p = new OwnPromise(function (resolve, reject) {
+  // if (true) {
+  setTimeout(() => {
+    console.log('basic promise')
+    resolve(0)
+  }, 2000);
+  // } else {reject('Error')}
+  //   reject()
+});
+
+
+
+
+// p.then(console.log('abc')) //будет ошибка, если если console.log(F()) внутри сет таймаут
+
+// =======================================
+
+// console.log(p
+// .then((data) => {console.log(data + 1); return data + 1})
+// .then((data) => {console.log(data + 1); return data + 1})
+// .then((data) => {console.log(data + 1); return data + 1})
+// )
+
+// =======================================
+
+// p.then(v => {
+//   console.log('1');
+// })
+//   .then(v => {
+//     console.log('4');
+//   });
+
+// p.then(v => {
+//   console.log('2');
+// })
+// p.then(v => {
+//     console.log('3');
+//   });
+
+// =======================================
+
+p.then((v) => {
+  // console.log(v,'first then 1');
+  // return 1;
+
+  const p1 = new OwnPromise(function (resolve, reject) {
+
+    setTimeout(() => {
+      console.log(v, 'first then 1');
+      resolve(1);
+    },
+      1000)
+
+  });
+
+  return p1.then(a => a * 2).then(a => a * 3)
+
+}).then((v) => {
+  console.log(v, 'second after first then 4');
+  return 2;
+});
+
+p.then((v) => {
+  console.log(v, 'first independed then 2');
+
+});
+p.then((v) => {
+  console.log(v, 'second independed then 3');
+});
